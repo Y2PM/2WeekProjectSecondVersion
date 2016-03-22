759fd9fabe68a91634a9f45bd4dd7f7264569b5a
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-22 12:50:19 +0000

    wcf service cl

--- WCFServiceCL/Create/CreateGameService.cs
+++ WCFServiceCL/Create/CreateGameService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WCFServiceCL
+namespace WCFServiceCL.Create
 {
     public class CreateGameService : ICreateGameService
     {
