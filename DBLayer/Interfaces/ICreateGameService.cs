c281350783cc4667d8acccafabd16b00aae1a183
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-22 11:31:07 +0000

    added a bit of service...

---
+++ DBLayer/Interfaces/ICreateGameService.cs
@@ -0,0 +1,17 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLayer.Interfaces
+{
+    [ServiceContract]
+    public interface ICreateGameService
+    {
+        [OperationContract]
+        void CreateGameMethod(Game game);
+    }
+}
