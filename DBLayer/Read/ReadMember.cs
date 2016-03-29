bd6e9b27bc046341ec185c4246e6afb593fbff2a
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-29 15:08:02 +0100

    Joe's connection strings

--- DBLayer/Read/ReadMember.cs
+++ DBLayer/Read/ReadMember.cs
@@ -38,29 +38,29 @@ namespace DBLayer.Read
         public bool login(string user, string pass)
         {
             int memid = 0;
-            
-                var memberidquery = (from e in context.Members
-                                     where e.m_username == user
-                                     select e.member_id);
-                foreach (var mem in memberidquery)
-                {
-                    memid = Convert.ToInt32(mem);
-                }
 
-                var m = context.Members.Find(memid);
-                if (memid == 0)
-                {
-                    return false;
-                }
-                if (m.m_password == pass)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            
+            var memberidquery = (from e in context.Members
+                                 where e.m_username == user
+                                 select e.member_id);
+            foreach (var mem in memberidquery)
+            {
+                memid = Convert.ToInt32(mem);
+            }
+
+            var m = context.Members.Find(memid);
+            if (memid == 0)
+            {
+                return false;
+            }
+            if (m.m_password == pass)
+            {
+                //Login sucsessful
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public decimal ReadMemberAccount(int memberid)
