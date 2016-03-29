734e4533435c731133e4ff0fea69add8e4752cdd
Lt. <adanmadavid@hotmail.com>
2016-03-29 12:07:22 +0100

    asp home

--- ASP/App_Start/RouteConfig.cs
+++ ASP/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ASP
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MemberSide", action = "SignUp", id = UrlParameter.Optional }
             );
         }
     }
