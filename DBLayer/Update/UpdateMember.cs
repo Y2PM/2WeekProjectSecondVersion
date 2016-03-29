0a0ba1b8bfed3aac841574b1c97a570b78749945
cemal13 <cemalsc@gmail.com>
2016-03-29 10:46:56 +0100

    price added

--- DBLayer/Update/UpdateMember.cs
+++ DBLayer/Update/UpdateMember.cs
@@ -1,9 +1,5 @@
-﻿using DBLayer.Read;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DBLayer.Update
 {
