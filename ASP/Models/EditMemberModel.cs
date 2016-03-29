b353766784b41a9f6214c8b8b044ad47202a61eb
Lt. <adanmadavid@hotmail.com>
2016-03-29 12:50:24 +0100

    edit member view

---
+++ ASP/Models/EditMemberModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.Models
+{
+    public class EditMemberModel
+    {
+        public string currentpassword { get; set; }
+        public string newpassword { get; set; }
+        public decimal addtobalance { get; set; }
+        public string passworderror { get; set; }
+    }
+}
