44dc0e3aa99d99a31f00ab311bf3a443ab1de9a4
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-30 14:29:13 +0100

    a

--- GroupProjectSQLQuery.sql
+++ GroupProjectSQLQuery.sql
@@ -1,7 +1,21 @@
 ﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Game]') AND type in (N'U'))
 DROP TABLE [dbo].[Game]
+
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Member]') AND type in (N'U'))
 DROP TABLE [dbo].[Member]
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[GroupProject].[dbo].[Log]') AND type in (N'U'))
+drop table [GroupProject].[dbo].[Log]
+
+CREATE TABLE [GroupProject].[dbo].[Log] (
+    [Id] [int] IDENTITY (1, 1) NOT NULL,
+    [Date] [datetime] NOT NULL,
+    [Thread] [varchar] (255) NOT NULL,
+    [Level] [varchar] (50) NOT NULL,
+    [Logger] [varchar] (255) NOT NULL,
+    [Message] [varchar] (4000) NOT NULL,
+    [Exception] [varchar] (2000) NULL
+)
 
 CREATE TABLE Game
 (
