fa0c442b4667471ed6119c0a5df9dd7b27fc5760
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-30 14:32:11 +0100

    sql query addition

--- GroupProjectSQLQuery.sql
+++ GroupProjectSQLQuery.sql
@@ -8,7 +8,7 @@ IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[GroupProjec
 drop table [GroupProject].[dbo].[Log]
 
 CREATE TABLE [GroupProject].[dbo].[Log] (
-    [Id] [int] IDENTITY (1, 1) NOT NULL,
+    [Id] [int] IDENTITY (1, 1) NOT NULL PRIMARY KEY,
     [Date] [datetime] NOT NULL,
     [Thread] [varchar] (255) NOT NULL,
     [Level] [varchar] (50) NOT NULL,
